e15887250be6fa200cd578d5275d848d31cd605c
AdepuShashank30-max <shashankadepu30@gmail.com>
2026-08-03 00:02:04 +0530

    Delete test1 directory

--- test1/hello.java
+++
@@ -1,7 +0,0 @@
-// A basic HelloWorld program in Java
-public class hello {
-    public static void main(String[] args) {
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
-    }
-}
