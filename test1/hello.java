91906f2e55dfcf5cb5369970eb6327596fe1a4fd
AdepuShashank30-max <shashankadepu30@gmail.com>
2026-08-02 23:24:29 +0530

    first commit

---
+++ test1/hello.java
@@ -0,0 +1,7 @@
+// A basic HelloWorld program in Java
+public class hello {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
